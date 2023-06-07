a5f144979ced37ec0d89c8af485b05749ec1b1bf
ReddyKousic <85285392+ReddyKousic@users.noreply.github.com>
2023-06-07 10:33:08 +0530

    Update fetch_data.php

--- home/fetch_data.php
+++ home/fetch_data.php
@@ -1,18 +1,15 @@
 <?php
 // Database connection details
-$host = 'db4free.net';
-$dbname = 'kousicreddy';
-$username = 'kousicreddy2211';
-$password = 'kousicreddy2211A!';
-
+$host = '<DATABASEHOSTNAME>';
+$dbname = '<DATABASENAME>';
+$username = '<DATABASEUSERNAME>';
+$password = '<DATABASEPASSWORD>';
 try {
     // Connect to the database
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // Fetch the updated data from the database
     $stmt = $pdo->query('SELECT * FROM posts order by date DESC');
-
     // Prepare the HTML markup for each row of data
     $output = '';
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -21,7 +18,6 @@ try {
         include('p.php'); // Include the p.php template
         $output .= ob_get_clean(); // Capture the output and append to the $output variable
     }
-
     // Send the HTML markup as the response
     echo $output;
 } catch (PDOException $e) {
